bc67c4fa3c4a1aa1df0e9001a4926ee370c8e4aa
leejinseok <sonaky47@gmail.com>
2024-03-15 08:04:42 +0900

    update - 뷸필요 클래스 제거

--- api/src/main/java/com/hdjunction/homework/api/exception/DuplicateException.java
+++
@@ -1,8 +0,0 @@
-package com.hdjunction.homework.api.exception;
-
-public class DuplicateException extends RuntimeException {
-
-    public DuplicateException(final String message) {
-        super(message);
-    }
-}
